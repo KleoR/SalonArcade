91bb0aab007a61f794a43e59f2b1f7055c02a3ed
Kleopatra Raynova <kleo.rayn@gmail.com>
2025-12-10 19:08:10 +0100

    Clase de msj, menus y grandes textos.

---
+++ src/main/java/service/Mensajes.java
@@ -0,0 +1,93 @@
+package service;
+
+public class Mensajes {
+
+    public static void mostrarMenu(){
+        System.out.println("""
+            =============================================================
+                          ★  A R C A D E   M A N A G E R  ★
+            =============================================================
+            
+               |  1 | Registrar nuevo jugador
+               |  2 | Registrar nueva máquina arcade
+               |  3 | Recargar créditos a un jugador
+               |  4 | Listar jugadores
+               |  5 | Listar máquinas
+               |  6 | Listar máquinas activas
+               |  7 | Mantenimiento / Reactivar máquina
+               |  8 | Jugar una partida
+               |  9 | Mostrar jugador más activo
+               | 10 | Mostrar máquina más usada
+               | 11 | Ranking de una máquina
+               | 12 | Dar de baja una máquina
+               | 13 | Editar máquina arcade
+               | 14 | Editar jugador
+          
+            -------------------------------------------------------------
+                             0.  █  S  A  L  I  R  █
+            =============================================================
+            """);
+    }
+
+//
+//        String menuV1 = ("""
+//            =============================================================
+//                                A R C A D E   M A N A G E R
+//            =============================================================
+//
+//               1. Jugadores
+//               2. Máquinas Arcade
+//               3. Partidas y Estadísticas
+//
+//            -------------------------------------------------------------
+//                             0.  █  S  A  L  I  R  █
+//            =============================================================
+//            """);
+//
+//        String menuV2 = ("""
+//            ====================  J U G A D O R E S  ====================
+//
+//               1. Registrar nuevo jugador
+//               2. Recargar créditos
+//               3. Editar jugador
+//               4. Listar jugadores
+//               5. Jugador más activo
+//
+//            -------------------------------------------------------------
+//               0. Volver al menú principal
+//            =============================================================
+//            """);
+//
+//        String menuV2 = ("""
+//            =================  M Á Q U I N A S   A R C A D E  =================
+//
+//               1. Registrar nueva máquina arcade
+//               2. Listar máquinas
+//               3. Listar máquinas activas
+//               4. Mantenimiento / Reactivar
+//               5. Dar de baja máquina
+//               6. Editar máquina
+//               7. Máquina más usada
+//               8. Ranking de una máquina
+//
+//            -------------------------------------------------------------------
+//               0. Volver al menú principal
+//            ===================================================================
+//            """);
+//
+//        String menuV2 = ("""
+//            ===================  P A R T I D A S  ===================
+//
+//               1. Jugar una partida
+//               2. Jugador más activo
+//               3. Máquina más usada
+//               4. Ranking de una máquina
+//
+//            ----------------------------------------------------------
+//               0. Volver al menú principal
+//            ==========================================================
+//            """);
+//
+
+    }
+
